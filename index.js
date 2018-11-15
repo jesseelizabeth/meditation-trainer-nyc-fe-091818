2e44e34110816ec7335eae53b39d44c85639e96d
Jesse Evans <jesseelizabeth1@gmail.com>
2018-11-15 13:55:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+// find button
+// add event listener for click
+// on click change the text
+// when button says finished, record time of breath in breath log 
+
+function buttonFunction() {
+  const input = document.querySelector('#new-breath')
+  const newBreath = input.duration
+  const breathLog = document.querySelector('#breath-log')
+  const listItem = document.createElement('li')
+  listItem.innerText = newBreath
+  breathLog.appendChild(listItem)
+  
+}
+
+const button = document.querySelector('#button')
+button.addEventListener('click', buttonFunction)
+button.innerText = 'INHALE DONE'
